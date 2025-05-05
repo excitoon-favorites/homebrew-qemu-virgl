eb1d6f25c592dd79fc794fd5d4ae753ef5a87670
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2025-05-05 12:16:15 +0300

    sdf

--- Formula/qemu-virgl.rb
+++ Formula/qemu-virgl.rb
@@ -1,8 +1,8 @@
 class QemuVirgl < Formula
   desc "Emulator for x86 and PowerPC"
   homepage "https://www.qemu.org/"
-  url "https://github.com/excitoon-favorites/qemu.git", using: :git, revision: "b1e35c760acd9a85a4469c69465815ec2e2e6eba"
-  version "20211212.79"
+  url "https://github.com/excitoon-favorites/qemu.git", using: :git, revision: "4d5d933bbc7cc52f6cc6b9021f91fa06266222d5"
+  version "20211212.80"
   license "GPL-2.0-only"
 
   depends_on "libtool" => :build
